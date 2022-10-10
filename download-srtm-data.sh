48df5188fd1fc1f2a49b1f89de738596033939c7
Volandoo App <112556315+volandoo@users.noreply.github.com>
2022-10-10 09:36:52 +0200

    Update download-srtm-data.sh

--- download-srtm-data.sh
+++ download-srtm-data.sh
@@ -4,6 +4,6 @@ set -eu
 wget https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_NE_250m_TIF.rar && \
 wget https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_SE_250m_TIF.rar && \
 wget https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_W_250m_TIF.rar && \
-unrar e -f -o+ SRTM_NE_250m_TIF.rar && \
-unrar e -f -o+ SRTM_SE_250m_TIF.rar && \
-unrar e -f -o+ SRTM_W_250m_TIF.rar
+unrar e -o+ SRTM_NE_250m_TIF.rar && \
+unrar e -o+ SRTM_SE_250m_TIF.rar && \
+unrar e -o+ SRTM_W_250m_TIF.rar
